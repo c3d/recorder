255652fcb5d3c36dc856cb716b74b721e02ea602
Christophe de Dinechin <dinechin@redhat.com>
2017-04-27 08:19:06 +0200

    Avoid unsatisfied symbols

--- recorder.cpp
+++ recorder.cpp
@@ -1,5 +1,5 @@
 // ****************************************************************************
-//  flight_recorder.cpp                                       Recorder project
+//  recorder.cpp                                             Recorder project
 // ****************************************************************************
 //
 //   File Description:
@@ -49,7 +49,7 @@
 
 // Define all the recorders we need
 #define RECORDER(Name, Size)                                    \
-extern Recorder<Name##_record, Name##_recorder_list> Name;
+Recorder<Name##_record, Name##_recorder_list> Name;
 #include "recorder.tbl"
 
 
