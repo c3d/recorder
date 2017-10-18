27271364fe39de60a68ee0b9230b57a342347687
Christophe de Dinechin <dinechin@redhat.com>
2017-10-18 23:14:22 +0200

    Add usage information and -h option

--- scope/recorder_scope.cpp
+++ scope/recorder_scope.cpp
@@ -28,6 +28,34 @@
 #include <QtWidgets/QVBoxLayout>
 
 
+void usage(const char *progname)
+// ----------------------------------------------------------------------------
+//   Display usage information about the program
+// ----------------------------------------------------------------------------
+{
+    printf("Usage: %s [[-c config][-s slider][chan_re]...]\n"
+           "\n"
+           "  Arguments:\n"
+           "    -c config       :  Send configuration command\n"
+           "    -s slider       : Setup a control slider\n"
+           "    chan_re         : Add view with channels matching regexp\n"
+           "\n"
+           "  Configuration syntax for -c matches RECORDER_TRACES syntax\n"
+           "  Slider syntax is slider[=value[:min:max]]\n"
+           "\n"
+           "  See http://github.com/c3d/recorder for more information\n"
+           "\n"
+           "  Examples of arguments:\n"
+           "    -c '.*errors'     : Enable display of all errors\n"
+           "    -c rate=10        : Set 'rate' tweak to 10\n"
+           "    -s rate=10:2:39   : Create slider to set 'rate'\n"
+           "                        initial value 10, range 2 to 39\n"
+           "    my_graph          : Show graph for channel 'my_graph'\n"
+           "    (min|max)_rate    : Show min_rate and max_rate graph\n",
+           progname);
+}
+
+
 int main(int argc, char *argv[])
 // ----------------------------------------------------------------------------
 //   Create the main widget for the oscilloscope and display it
@@ -50,9 +78,13 @@ int main(int argc, char *argv[])
     for (int a = 1; a < argc; a++)
     {
         QString arg = argv[a];
-        if (arg == "-c" && ++a < argc)
+        if (arg == "-h")
         {
-            if (!recorder_chans_configure(chans, argv[a]))
+            usage(argv[0]);
+        }
+        else if (arg == "-c" && a+1 < argc)
+        {
+            if (!recorder_chans_configure(chans, argv[++a]))
             {
                 fprintf(stderr, "Insufficient command space to send '%s'\n",
                         argv[a]);
@@ -60,15 +92,20 @@ int main(int argc, char *argv[])
             }
             configurations++;
         }
-        else if (arg == "-s" && ++a < argc)
+        else if (arg == "-s" && a+1 < argc)
         {
-            QGroupBox *slider = RecorderSlider::make(path, chans, argv[a]);
+            QGroupBox *slider = RecorderSlider::make(path, chans, argv[++a]);
             layout->addWidget(slider);
+        }
+        else if (arg[0] == '-')
+        {
+            fprintf(stderr, "Invalid option %s\n", argv[a]);
+            usage(argv[0]);
         }
         else
         {
-            RecorderView *recorderView = new RecorderView(path, chans, argv[a]);
-            layout->addWidget(recorderView);
+            RecorderView *view = new RecorderView(path, chans, argv[a]);
+            layout->addWidget(view);
             views++;
         }
     }
