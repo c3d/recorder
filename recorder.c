d9950a9fa9fc2b0cdd1237d59f9633a8ae1473e3
Christophe de Dinechin <dinechin@redhat.com>
2018-03-14 16:53:18 +0100

    Use "%+s" for safe strings instead of "%!s"
    This is more likely to be correctly interpreted by regular printf,
    which means it's easier to use the same format for the recorder
    and for some other printf-based tool.

--- recorder.c
+++ recorder.c
@@ -460,7 +460,6 @@ static void recorder_dump_entry(recorder_info      *rec,
                 case '0': case '1': case '2': case '3': case '4':
                 case '5': case '6': case '7': case '8': case '9':
                 case '.':
-                case '+':
                 case '-':
                 case 'l': case 'L':
                 case 'h':
@@ -470,9 +469,8 @@ static void recorder_dump_entry(recorder_info      *rec,
                 case 'q':
                 case 'v':
                     break;
-                case '!':
+                case '+':
                     safe_string = true;
-                    fmt_copy--;
                     break;
                 case 'n':           // Expect two args
                 case '*':
